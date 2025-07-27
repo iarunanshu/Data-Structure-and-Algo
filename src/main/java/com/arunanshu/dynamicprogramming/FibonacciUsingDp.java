05fa12138a9e84405830e8c716e85439f2aad6d9
Arunanshu Jena <arunanshu_jena@epam.com>
2025-07-27 20:38:50 +0530

    dpq1 update

---
+++ src/main/java/com/arunanshu/dynamicprogramming/FibonacciUsingDp.java
@@ -0,0 +1,22 @@
+package com.arunanshu.dynamicprogramming;
+
+import java.util.Arrays;
+
+public class FibonacciUsingDp {
+    public static void main(String[] args) {
+        int[] arr=new int[10];
+        Arrays.fill(arr,-1);
+        System.out.println(fibanacci(arr,7));
+    }
+    public static int fibanacci(int[] dp,int n ){
+        if (dp[n]==-1){
+            int res;
+            if(n==1 || n==0){
+                return n;
+            }
+            else res=fibanacci(dp,n-1)+fibanacci(dp,n-2);
+            dp[n]=res;
+        }
+        return dp[n];
+    }
+}
