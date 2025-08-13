0da1b23945862fde84c63cb05e1d7466bc1412d0
Arunanshu Jena <arunanshuj@gmail.com>
2025-08-13 17:55:54 +0530

    initial commit

--- src/main/java/com/arunanshu/hashing/SubArrayWithZeroSum.java
+++ src/main/java/com/arunanshu/hashing/SubArrayWithZeroSum.java
@@ -1,8 +1,23 @@
 package com.arunanshu.hashing;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SubArrayWithZeroSum {
     public static void main(String[] args) {
-
+        int[] arr={10,20,12,-12,56,34,-20};
+        System.out.println(zeroSum(arr));
+    }
+    public static Boolean zeroSum(int arr[]){
+        Set<Integer>st=new HashSet<>();
+        int sum=0;
+        for (int i = 0; i < arr.length; i++) {
+            sum+=arr[i];
+            if(st.contains(sum)||sum==0)
+                return true;
+            st.add(sum);
+        }
+        return false;
     }
 
 }
